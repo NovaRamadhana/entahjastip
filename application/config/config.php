0c467dd3298f683dfa778d831e858cc63d98bcb2
Hanraf <92284509+Hanraf@users.noreply.github.com>
2023-12-01 05:57:05 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/entahjastip/';
+$config['base_url'] = 'http://entahjastip.000webhostapp.com/';
 
 /*
 |--------------------------------------------------------------------------
